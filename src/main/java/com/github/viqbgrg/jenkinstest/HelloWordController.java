0fde5e5cadc094030b7dbb31c0a6f210192d8dc5
viqbgrg <viqbgrg@gmail.com>
2020-11-25 10:16:15 +0800

    Jenkinsfile

---
+++ src/main/java/com/github/viqbgrg/jenkinstest/HelloWordController.java
@@ -0,0 +1,16 @@
+package com.github.viqbgrg.jenkinstest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloWordController {
+    @GetMapping("")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
